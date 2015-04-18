b7645cede3e5180188aa396ef8bd294ea25759e2
GIT Admin <jorges119@hotmail.com>
2015-04-18 23:26:33 +0000

    Changed sample code to exit on fingers spread

--- Examples/Sample.py
+++ Examples/Sample.py
@@ -1,6 +1,10 @@
 import myo
 import time
 
+#----------------------------------------
+# Spread fingers to exit the application
+#----------------------------------------
+
 n=myo.myMyo("E1:23:E9:52:9F:DB")
 n.setVibration(myo.myMyo.vibration["MEDIUM"])
 
@@ -12,7 +16,8 @@ while True:
     pose = n.getPoseData()["POSE"]
     if pose == myo.myMyo.pose["FIST"]:
 	print "Fist Closed"
-    
+    elif pose == myo.myMyo.pose["FINGERS__SPREAD"]:   
+        break
     time.sleep(0.5)
 
 n.disconnect()
